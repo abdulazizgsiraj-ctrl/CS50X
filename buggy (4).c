89e197bf2e514497aa2990ed6409a085ff209d91
abdulazizgsiraj-ctrl <abdulazizgsiraj@gmail.com>
2026-09-09 17:55:38 +0300

    Add files via upload

---
+++ buggy (4).c
@@ -0,0 +1,18 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void print_column(int heigtht)
+
+int main(void)
+{
+    int h = get_int("height: ");
+    print_column(h);
+}
+
+void print_column(int heigtht)
+{
+    for (int i = 0; i <= height; i++)
+{
+    printf("#\n")
+    }
+}
